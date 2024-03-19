7ccd4d8346bad39187c38ea77c7c063c4f6dd014
EverardoG <everardo.a.gonzalez@gmail.com>
2024-03-18 20:59:22 -0700

    comp mod

--- src/evolutionary-algorithms/learn.py
+++ src/evolutionary-algorithms/learn.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import os
 from julia.api import Julia
 # Julia(runtime=os.path.expanduser("~")+'/julia-1.8.2/bin/julia-1.8.2',compiled_modules=False)
-Julia(compiled_mdules=False)
+Julia(compiled_modules=False)
 from julia import Main
 import numpy as np
 
